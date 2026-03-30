b78e04ef440242ada741c4f157c9f216be101299
Arturo <arturo.seri.garrido@gmail.com>
2026-03-30 20:02:01 +0200

    he cambiado nombres de fotos

--- javascript/PaginaScript.js
+++ javascript/PaginaScript.js
@@ -1,6 +1,18 @@
 let fotos = [
-    { id: 1, url: "../assets/imagenes/cuervo.jpg", categorias: ["Terrestre", "Aérea"] },
-    { id: 2, url: "../assets/fotos/foto2.jpg", categorias: ["Acuática"] },
-    { id: 3, url: "../assets/fotos/foto3.jpg", categorias: ["Terrestre"] },
-    { id: 4, url: "../assets/fotos/foto4.jpg", categorias: ["Acuática"] }, 
+    { id: 1, url: "../assets/imagenes/ajolote.jpg", categorias: ["Terrestre", "Aérea"] },
+    { id: 2, url: "../assets/imagenes/ballena.jpg", categorias: ["Acuática"] },
+    { id: 3, url: "../assets/imagenes/cerdo.jpg", categorias: ["Terrestre"] },
+    { id: 4, url: "../assets/imagenes/Ciervo.jpg", categorias: ["Acuática"] }, 
+    { id: 5, url: "../assets/imagenes/Cuervo.jpg", categorias: ["Aérea"] },
+    { id: 6, url: "../assets/imagenes/Delfin.jpg", categorias: ["Terrestre", "Acuática"] },
+    { id: 7, url: "../assets/imagenes/gaviota.jpg", categorias: ["Aérea"] },
+    { id: 8, url: "../assets/imagenes/foto8.jpg", categorias: ["Terrestre"] },
+    { id: 9, url: "../assets/imagenes/foto9.jpg", categorias: ["Acuática"] },
+    { id: 10, url: "../assets/imagenes/foto10.jpg", categorias: ["Aérea", "Terrestre"] },
+    { id: 11, url: "../assets/imagenes/foto11.jpg", categorias: ["Acuática"] },
+    { id: 12, url: "../assets/imagenes/foto12.jpg", categorias: ["Terrestre"] },
+    { id: 13, url: "../assets/imagenes/foto13.jpg", categorias: ["Aérea"] },
+    { id: 14, url: "../assets/imagenes/foto14.jpg", categorias: ["Acuática"] },
+    { id: 15, url: "../assets/imagenes/foto15.jpg", categorias: ["Terrestre", "Aérea"] }
+    
 ] 

1d0aa90434cc98e746b2ada282e9a73039b5e275
Arturo <arturo.seri.garrido@gmail.com>
2026-03-30 19:48:17 +0200

    no se que cambios he hecho, Hola Marc

--- javascript/PaginaScript.js
+++ javascript/PaginaScript.js
@@ -1,1 +1,6 @@
-//hola hola
+let fotos = [
+    { id: 1, url: "../assets/imagenes/cuervo.jpg", categorias: ["Terrestre", "Aérea"] },
+    { id: 2, url: "../assets/fotos/foto2.jpg", categorias: ["Acuática"] },
+    { id: 3, url: "../assets/fotos/foto3.jpg", categorias: ["Terrestre"] },
+    { id: 4, url: "../assets/fotos/foto4.jpg", categorias: ["Acuática"] }, 
+] 
